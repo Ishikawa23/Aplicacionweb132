67fc270778e221a7e38b75f5488fd4394c53909b
Peter Rivera <peterrivera2324@hotmail.com>
2022-06-30 14:47:23 -0500

    Se soluciona conflicto

--- calcular.php
+++ calcular.php
@@ -3,16 +3,6 @@ $cantidad = $_POST['cantidad'];
 $subtotal = $cantidad*125;
 $numero_cajas = ceil($cantidad/4);
 $flete = $numero_cajas*50;
-<<<<<<< HEAD
-if ($cantidad>1000)
-    $descuento=$subtotal*0.15;
-else
-{
-    if($cantidad>100)
-        $descuento=$subtotal*0.05;
-    else
-        $descuento=0;
-=======
 if ($cantidad>1000){    
     $descuento=$subtotal*0.15;
 }
@@ -25,15 +15,11 @@ else
     else{   
         $descuento=0;
     }
->>>>>>> issue2
 }
 $total=$subtotal+$flete-$descuento;
 echo "El precio total a pagar es: ".$total."<br>";
 echo "El monto por flete es: ".$flete."<br>";
 echo "El descuento aplicado fue: ".$descuento;
-<<<<<<< HEAD
-?>
-=======
 
 $edad = 24;
 switch($edad)
@@ -52,4 +38,3 @@ switch($edad)
 
 ?>
 
->>>>>>> issue2
